0b8aff769528183bd5d52c48aaac4b25500cc1ca
Stormi_139 <80914243+RealStormi@users.noreply.github.com>
2021-05-03 13:33:34 +0200

    Update src/de.stormi/commands/info.java

--- src/de.stormi/commands/info.java
+++ src/de.stormi/commands/info.java
@@ -1,18 +1,1 @@
-package de.stormi.commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-public class InfoCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            player.sendMessage("Current Version: 1");
-        } else
-            commandSender.sendMessage("§4§lYou can only use the Command Ingame!");
-        return false;
-    }
-}
