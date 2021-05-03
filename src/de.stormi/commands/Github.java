ad2f6668ac2b6bbf272029ad76c6036a7f2cf84b
Stormi_139 <80914243+RealStormi@users.noreply.github.com>
2021-05-03 13:33:10 +0200

    Update src/de.stormi/commands/Github.java

--- src/de.stormi/commands/Github.java
+++ src/de.stormi/commands/Github.java
@@ -1,18 +1,1 @@
-package de.stormi.commands;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
-public class github implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if(commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-            player.sendMessage(Github: https://github.com/RealStormi/guard ");
-        } else
-            commandSender.sendMessage("§4§lDu darfst diesen Befehl nur im Spiel benutzen!");
-        return false;
-    }
-}
